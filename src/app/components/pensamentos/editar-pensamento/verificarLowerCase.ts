ef71d90631340da31d447d5a0e417e088c5f75bb
Bruno Pocay <brunopocay@gmail.com>
2023-06-13 01:16:13 -0300

    avançando no curso de angular

---
+++ src/app/components/pensamentos/editar-pensamento/verificarLowerCase.ts
@@ -0,0 +1,12 @@
+import { AbstractControl } from "@angular/forms";
+
+export function watchLowerCase(control: AbstractControl) {
+
+    const autoria = control.value as string;
+
+    if(autoria !== autoria?.toLowerCase()) {
+        return { minusculo: true };
+    } 
+    else
+        return null;
+}
